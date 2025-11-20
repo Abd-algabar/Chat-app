2c59f3390fc97555209cd603b7dc8045bfe0c4a7
Abd-algabar <engabdalgabar@gmail.com>
2025-11-20 14:42:24 +0300

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const BASE_URL=import.meta.env.BASE_URL
 export const axiosInstance=axios.create(
     {
-        baseURL:"http://localhost:5000/api",
+        baseURL:"https://chat-app-ksg1.onrender.com/api",
         withCredentials:true, //send cookies with the request
     }
 )

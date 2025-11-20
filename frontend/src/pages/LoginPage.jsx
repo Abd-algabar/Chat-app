f0f18588647714111988cecc9b19c5e7b996ec2d
Abd-algabar <engabdalgabar@gmail.com>
2025-11-20 14:54:36 +0300

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { login } from "../lib/api";
 import { ShipWheelIcon } from "lucide-react";
 import { Link } from "react-router";
-import useLogin from "../hooks/uselogin";
+import useLogin from "../hooks/useLogin.js";
 
 const LoginPage = () => {
   const [loginData, setLoginData] = useState({
